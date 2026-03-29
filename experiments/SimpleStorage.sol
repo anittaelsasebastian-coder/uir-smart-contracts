1f87f74814c6adbdb724ff29fa4e0292b17a3f53
Anitta elsa sebastian <anittaelssebastian@Anittas-MacBook-Air.local>
2026-03-29 05:24:53 -0700

    Add SimpleStorage contract to experiments folder

--- experiments/SimpleStorage.sol
+++ experiments/SimpleStorage.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.0;
 
 contract SimpleStorage {
     address public owner;
-    unit256 public storedValue;
-    constructor(){
+    uint256 public storedValue;
+    constructor() {
         owner = msg.sender;
         storedValue = 0;
     }
-    function store(unit256 value) public{
+    function store(uint256 _value) public {
         require(msg.sender == owner, "only owner can store");
         storedValue = _value;
     }
-    function retrieve()publicview returns (unit256){
+    function retrieve() public view returns (uint256) {
         return storedValue;
     }
 }
