ef864fb2e7615e18961fb127951ef7f8fc8be2e3
Anitta elsa sebastian <anittaelssebastian@Anittas-MacBook-Air.local>
2026-04-02 01:13:43 -0700

    Simplify SimpleStorage to baseline PB-1 only per supervisor feedback

--- experiments/SimpleStorage.sol
+++ experiments/SimpleStorage.sol
@@ -2,14 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    address public owner;
     uint256 public storedValue;
-    constructor() {
-        owner = msg.sender;
-        storedValue = 0;
-    }
+
     function store(uint256 _value) public {
-        require(msg.sender == owner, "only owner can store");
         storedValue = _value;
     }
     function retrieve() public view returns (uint256) {
